751642f87e53677281682f45ed2ed04975695d17
ReaP <67383323+Peribunt@users.noreply.github.com>
2022-02-03 17:47:25 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -38,7 +38,7 @@ namespace ReturnSpoofer
 		FunctionToCall = Function;
 		FakeReturnAddress = FakeRet;
 		
-		( ( _RET_TYPE_( * )( IN OUT ... OPTIONAL ) )&SpoofCall )
+		return ( ( _RET_TYPE_( * )( IN OUT ... OPTIONAL ) )&SpoofCall )
 			( Args... );
 	}
 }
