8bc3672be41d2eb1de13b8f4eff1643a559447aa
ReaP <67383323+Peribunt@users.noreply.github.com>
2022-02-10 21:49:49 +0100

    Update Shellcode.asm

--- Shellcode.asm
+++ Shellcode.asm
@@ -1,8 +1,14 @@
 .code
 
+InitSpoofCall PROC
+mov rsi, rcx
+mov rdi, rdx
+ret
+InitSpoofCall ENDP
+
 SpoofCall PROC
 mov rax, 0deadbeef00000001h ;Move a recognizable magic number into RAX
-mov qword ptr[ rax ], 0     ;Deliberately cause an access violation to trap to our handler
+sti							;Deliberately cause an access violation to trap to our handler
 SpoofCall ENDP
 
 END
